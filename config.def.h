c639922b1da38e49da4b82617dcf389fbc75a421
seelts <seelts@gmail.com>
2024-08-30 15:31:32 +0200

    change font

--- config.def.h
+++ config.def.h
@@ -1,12 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
+#define FONT "UbuntuSansMonoNerdFontPropo-Regular:pixelsize=18:antialias=true:autohint=true"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 4;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { FONT };
+static const char dmenufont[]       = FONT;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
